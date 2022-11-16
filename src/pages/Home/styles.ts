54304a283fef4983c4b270f78e372125b27cfca3
Maiara santo <maiarasantos34@yahoo.com>
2022-11-16 14:14:51 -0300

    config5

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,27 +2,32 @@ import styled from "styled-components";
 
 export const ResumoProfissional = styled.h1`
 
-width: 990px;
-height: 40px;
-left: 325px;
-top: 150px;
 
+  width: 50.1rem;
+  height: 76px;
+  color: #ffffff;
+  font-size: 1.4rem;
+  margin: 2rem;
+  padding: 0 auto;
+  text-align: center;
+  height: 60px;
+  left: 325px;
+  top: 150px;
+  width: 890px;
+font-style: normal;
 font-family: 'Inter';
 font-style: normal;
 font-weight: 500;
 font-size: 25px;
-line-height: 150%;
+line-height: 120%;
 font-size: 2.0rem;
-/* or 38px */
-
 text-align: center;
 
 color: #FFFFFF;
 
-margin: 74px 0 14px;
-font-weight: 500;
 
-margin: 4rem;
+
+
 
 `;
 export const Container_content = styled.section`
