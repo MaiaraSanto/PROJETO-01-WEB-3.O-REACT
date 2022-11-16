941c0deb768cca9a48144e67b10b46c6b9028823
Maiara santo <maiarasantos34@yahoo.com>
2022-11-16 12:16:24 -0300

    initial commit

---
+++ src/pages/Home/styled.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const ResumoProfissional = styled.h1`
+
+width: 791px;
+height: 76px;
+left: 325px;
+top: 168px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 500;
+font-size: 27px;
+line-height: 160%;
+/* or 38px */
+
+text-align: center;
+
+color: #FFFFFF;
+
+margin: 84px 0 24px;
+font-weight: 700;
+
+margin: 5rem;
+
+`;
+export const Container_content = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
---
+++ src/shared/components/Botao/styled.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const ContainerButton = styled.button`
+  /* width: 12.3rem; */
+  height: 5.1rem;
+  background: #27272a;
+  border-radius: 0.8rem;
+  border: none;
+  padding: 1.5rem;
+  margin-left: 4rem;
+
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  gap: 0.5rem;
+
+  border: 1px solid #9d4b00;
+
+  img {
+    width: 1.8rem;
+    height: 1.8rem;
+    border-radius: 50%;
+  }
+  p {
+    font-size: 1.4rem;
+    color: #ededed;
+  }
+`;
